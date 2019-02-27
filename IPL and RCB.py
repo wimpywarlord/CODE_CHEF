1ebc4ddaa2c892665e39bc491ad3a37a5f5b614f
wimpywarlord <wimpywarlord@gmail.com>
2019-02-27 20:57:07 +0530

    CORRECT_IPL_AND_TEAMS

--- IPL and RCB.py
+++ IPL and RCB.py
@@ -1,7 +1,15 @@
 t=int(input())
 for i in range(0,t):
     p,n=map(int,input().split())
-    while n*2>p:
-        n-=1
-    print(n)
-        
+    if p<n:
+        print(0)
+    else:
+        win=n
+        score=2*win
+        #print(score)
+        while score!=p:
+            score-=1
+            win-=1
+        print(win)
+            
+            
