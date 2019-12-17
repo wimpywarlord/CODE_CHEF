d28e9e2f0d591acbe0b67365df4fa19564f25368
wimpywarlord <wimpywarlord@gmail.com>
2019-12-17 11:15:39 +0530

    made teh suplay on c++

--- Easy Subsequence Selection.py
+++ Easy Subsequence Selection.py
@@ -5,14 +5,15 @@ for i in range(0,int(input())):
     for j in range(n,0,-1):
         for k in range(j-1,0,-1):
             if s[j-1]==s[k-1]:
-                #print(k,j,s[k-1],s[j-1])
+                #print(k,j,n-j,s[k-1],s[j-1])
                 use.append(k+(n-j))
                 break
     if not use:
         print(0)
     else:
         print(max(use))
-                 
+
+'''
 for i in range(0,int(input())):
     n=int(input())
     s=input()
@@ -23,5 +24,5 @@ for i in range(0,int(input())):
                 if use<k+(n-j):
                     use=(k+(n-j))
                     break
-    print(use)
+    print(use)'''
 
