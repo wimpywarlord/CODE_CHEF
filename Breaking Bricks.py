b66f752a20eb8cc708db6f81bb87e7e52d0b9a09
wimpywarlord <wimpywarlord@gmail.com>
2020-01-05 14:58:53 +0530

    added breaking bricls

---
+++ Breaking Bricks.py
@@ -0,0 +1,137 @@
+import math
+for i in range(0,int(input())):
+    s,w1,w2,w3 = map(int,input().split())
+    if s==1 and w1==w2==w3==2:
+        print(6)
+    if s==2 and w1==w2==w3==2:
+        print(3)
+    if s==3 and w1==w2==w3==2:
+        print(3)
+    if s==4 and w1==w2==w3==2:
+        print(2)
+    if s==5 and w1==w2==w3==2:
+        print(2)
+    if s==6 and w1==w2==w3==2:
+        print(1)
+    if s==7 and w1==w2==w3==2:
+        print(1)
+    if s==8 and w1==w2==w3==2:
+        print(1)
+    if s==1 and w1==w2==w3==1:
+        print(3)
+    if s==2 and w1==w2==w3==1:
+        print(2)
+    if s==3 and w1==w2==w3==1:
+        print(1)
+    if s==4 and w1==w2==w3==1:
+        print(1)
+    if s==5 and w1==w2==w3==1:
+        print(1)
+    if s==6 and w1==w2==w3==1:
+        print(1)
+    if s==7 and w1==w2==w3==1:
+        print(1)
+    if s==8 and w1==w2==w3==1:
+        print(1)
+
+    if s==1 and w1==2 and w2==1 and w3==2:
+        print(5)
+    if s==2 and w1==2 and w2==1 and w3==2:
+        print(3)
+    if s==3 and w1==2 and w2==1 and w3==2:
+        print(2)
+    if s==4 and w1==2 and w2==1 and w3==2:
+        print(2)
+    if s==5 and w1==2 and w2==1 and w3==2:
+        print(1)
+    if s==6 and w1==2 and w2==1 and w3==2:
+        print(1)
+    if s==7 and w1==2 and w2==1 and w3==2:
+        print(1)
+    if s==8 and w1==2 and w2==1 and w3==2:
+        print(1)
+
+    if s==1 and w1==1 and w2==2 and w3==2:
+        print(5)
+    if s==2 and w1==1 and w2==2 and w3==2:
+        print(3)
+    if s==3 and w1==1 and w2==2 and w3==2:
+        print(2)
+    if s==4 and w1==1 and w2==2 and w3==2:
+        print(2)
+    if s==5 and w1==1 and w2==2 and w3==2:
+        print(1)
+    if s==6 and w1==1 and w2==2 and w3==2:
+        print(1)
+    if s==7 and w1==1 and w2==2 and w3==2:
+        print(1)
+    if s==8 and w1==1 and w2==2 and w3==2:
+        print(1)
+
+    if s==1 and w1==2 and w2==2 and w3==1:
+        print(5)
+    if s==2 and w1==2 and w2==2 and w3==1:
+        print(3)
+    if s==3 and w1==2 and w2==2 and w3==1:
+        print(2)
+    if s==4 and w1==2 and w2==2 and w3==1:
+        print(2)
+    if s==5 and w1==2 and w2==2 and w3==1:
+        print(1)
+    if s==6 and w1==2 and w2==2 and w3==1:
+        print(1)
+    if s==7 and w1==2 and w2==2 and w3==1:
+        print(1)
+    if s==8 and w1==2 and w2==2 and w3==1:
+        print(1)
+
+    if s==1 and w1==1 and w2==2 and w3==1:
+        print(4)
+    if s==2 and w1==1 and w2==2 and w3==1:
+        print(3)
+    if s==3 and w1==1 and w2==2 and w3==1:
+        print(2)
+    if s==4 and w1==1 and w2==2 and w3==1:
+        print(1)
+    if s==5 and w1==1 and w2==2 and w3==1:
+        print(1)
+    if s==6 and w1==1 and w2==2 and w3==1:
+        print(1)
+    if s==7 and w1==1 and w2==2 and w3==1:
+        print(1)
+    if s==8 and w1==1 and w2==2 and w3==1:
+        print(1)
+
+    if s==1 and w1==1 and w2==1 and w3==2:
+        print(4)
+    if s==2 and w1==1 and w2==1 and w3==2:
+        print(2)
+    if s==3 and w1==1 and w2==1 and w3==2:
+        print(2)
+    if s==4 and w1==1 and w2==1 and w3==2:
+        print(1)
+    if s==5 and w1==1 and w2==1 and w3==2:
+        print(1)
+    if s==6 and w1==1 and w2==1 and w3==2:
+        print(1)
+    if s==7 and w1==1 and w2==1 and w3==2:
+        print(1)
+    if s==8 and w1==1 and w2==1 and w3==2:
+        print(1)
+
+    if s==1 and w1==2 and w2==1 and w3==1:
+        print(4)
+    if s==2 and w1==2 and w2==1 and w3==1:
+        print(2)
+    if s==3 and w1==2 and w2==1 and w3==1:
+        print(2)
+    if s==4 and w1==2 and w2==1 and w3==1:
+        print(1)
+    if s==5 and w1==2 and w2==1 and w3==1:
+        print(1)
+    if s==6 and w1==2 and w2==1 and w3==1:
+        print(1)
+    if s==7 and w1==2 and w2==1 and w3==1:
+        print(1)
+    if s==8 and w1==2 and w2==1 and w3==1:
+        print(1)
