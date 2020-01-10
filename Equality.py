9c2e552bc68097d9072c06f3645dd490112943b9
wimpywarlord <wimpywarlord@gmail.com>
2020-01-10 22:45:54 +0530

    adde only one test case pass Equality

--- Equality.py
+++ Equality.py
@@ -1,11 +1,25 @@
 n,q=map(int,input().split())
-print(n,q)
-arr = list(map(int,input().split()))
-print(arr)
+#print(n,q)
+a = list(map(int,input().split()))
+#print(a)
 for i in range(0,q):
     l,r=map(int,input().split())
     l=l-1
     r=r-1
-    print(l,r)
-    for j in range(l,r+1):
+    
+    signal = 0 #0 IS DECREASING.
+    
+    signal = 1 #1 IS INCREASING.
+    if a[l]>a[l+1]:
+        signal = 0
+        for j in range(l,r):
+            
+        
+
+
+    
+    if i_ctr==d_ctr:
+        print("YES")
+    else:
+        print("NO")
         
